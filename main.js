9afed88e18be0452ca0a7a501a6480a533b7f0d5
Cameron Edwards <cameronprattedwards@gmail.com>
2014-12-18 18:05:32 -0500

    Add accordions

--- main.js
+++ main.js
@@ -26,6 +26,111 @@ require(["angular", "nav/nav", "angular-ui-router", "bootstrap"], function(angul
 		.controller("navCtrl", function($scope) {
 			$scope.nav = nav;
 		})
+		.directive("resume", function() {
+			return {
+				restrict: "A",
+				link: function(scope) {
+					scope.open = true;
+					scope.message = function() {
+						var allOpen = scope.accordions.every(function(accordion) {
+							return accordion.isOpen;
+						});
+						var allClosed = scope.accordions.every(function(accordion) {
+							return !accordion.isOpen;
+						});
+						if (allOpen) {
+							scope.open = true;
+							return "Collapse";
+						}
+						if (allClosed) {
+							scope.open = false;
+							return "Open";
+						}
+						return scope.open ? "Collapse" : "Open";
+					};
+				},
+				controller: function($scope) {
+					var accordions = [];
+					$scope.accordions = accordions;
+					this.addAccordion = function(accordion) {
+						accordions.push(accordion);
+					};
+					$scope.closeAll = function() {
+						accordions.forEach(function(accordion) {
+							accordion.close();
+						});
+					};
+					$scope.openAll = function() {
+						accordions.forEach(function(accordion) {
+							accordion.open();
+						});
+					};
+					$scope.toggle = function() {
+						if ($scope.open)
+							$scope.closeAll();
+						else
+							$scope.openAll();
+						$scope.open = !$scope.open;
+					}
+				}
+			};
+		})
+		.directive("accordion", function() {
+			return {
+				require: "^resume",
+				restrict: "A",
+				scope: {
+					open: "="
+				},
+				transclude: true,
+				templateUrl: "/accordion.html",
+				link: function(scope, elem, attrs, resume) {
+					resume.addAccordion(scope.ctrl);
+				},
+				controller: function($scope, $timeout) {
+					$scope.accordion = {
+						open: $scope.open
+					};
+					$scope.ctrl = this;
+					this.isOpen = $scope.open;
+					this.toggle = function() {
+						var _self = this;
+						$timeout(function() {
+							$scope.accordion.open = !$scope.accordion.open;
+							_self.isOpen = $scope.accordion.open;
+						});
+					};
+					this.open = function() {
+						var _self = this;
+						$timeout(function() {
+							$scope.accordion.open = true;
+							_self.isOpen = true;
+						});
+					};
+					this.close = function() {
+						var _self = this;
+						$timeout(function() {
+							$scope.accordion.open = false;
+							_self.isOpen = false;
+						});
+					};
+				}
+			};
+		})
+		.directive("accordionTrigger", function($timeout) {
+			return {
+				restrict: "A",
+				require: "^accordion",
+				link: function(scope, element, attrs, accordion) {
+					element
+						.append("<i class=\"fa fa-chevron-up\"></i><i class=\"fa fa-chevron-down\"></i>")
+						.bind("click", function() {
+							accordion.toggle();
+						})
+						.addClass("text-muted").addClass("pull-right");
+				}
+			}
+		})
 		.config(function($urlRouterProvider, $stateProvider) {
 			$urlRouterProvider.otherwise("/about");
 
